78017952c810cf3e10680ab1ae56f1bccd15b1ad
ppeach <mail@ppeach.com>
2022-05-08 08:46:52 +1000

    Added explainer for DIY devices

--- index.php
+++ index.php
@@ -233,7 +233,12 @@ if(isset($_GET['submit'])){
                     </div>
                 </div-->
                 <div class="col-md-6">
-                    <label for="diy" class="form-label">Include DIY filters?</label>
+                    <label for="diy" class="form-label">Include DIY devices?</label>
+			<div>
+                        <a href="#" data-bs-trigger="hover focus" data-bs-toggle="popover" title="What is a DIY model?" data-bs-content="Do it yourself air filters are alternatives to commercially available devices that, when properly constructed, can be an economical and effective alternative. Clean air delivery rates (CADR) for these devices in a way that can be compared directly to AHAM CADR Smoke tested devices are currently estimated only." data-bs-html="true">
+                            <p>What is a DIY device? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16"><path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"></path></svg></p>
+                        </a>
+					</div>
                     <select class="form-select" id="diy" name="diy" required>
                         <option value="No" <?php if($diy == 'No' || !$submitted) {echo 'selected';} ?> >No</option>
                         <option value="Yes" <?php if($diy == 'Yes') {echo 'selected';} ?> >Yes</option>
