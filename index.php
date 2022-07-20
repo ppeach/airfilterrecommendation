ec1094e4fcea3c28fa4e3d2763f086900ab15f55
ppeach <mail@ppeach.com>
2022-07-21 08:56:12 +1000

    Updated noise level recommendations

--- index.php
+++ index.php
@@ -192,9 +192,9 @@ if(isset($_GET['submit'])){
 					<div>
                         <a href="#" data-bs-trigger="hover focus" data-bs-toggle="popover" title="Decibel (dBA) limit guide" data-bs-content="Tolerability of noise from air filters depends greatly on existing ambient noise levels. The below recommendations are a guide only. <ul class='list-group list-group-flush'>
                                                     <li class='list-group-item'>30-35dBA Sleep</li>
-                                                    <li class='list-group-item'>40-50dBA Classroom</li>
-                                                    <li class='list-group-item'><50dBA Quiet Restaurant, Office</li>
-                                                    <li class='list-group-item'><60dBA Loud Restaurant, Gym, Childcare</li>
+                                                    <li class='list-group-item'>40-45dBA Classroom, Quiet Restaurant & Office</li>
+                                                    <li class='list-group-item'>40-50dBA Loud Office & Childcare</li>
+                                                    <li class='list-group-item'><60dBA Loud Restaurant, Gym</li>
                                                     <li class='list-group-item'>No Maximum - Acceptable for loud environments </li>
                                                     </ul>" data-bs-html="true">
                             <p>Noise limit guide <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16"><path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"></path></svg></p>
